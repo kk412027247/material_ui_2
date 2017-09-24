c60306913fac80871219e97f4d05b67675b71a95
TMD <1@123.com>
2017-09-24 19:09:10 +0800

    bottomNavigation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from './Avatar';
+import App from './BottomNavigation';
 
 
 
